2037cef87d12751df355795bb561649a62e12ad3
SkylinePixelStudio <krishiv.advt@gmail.com>
2026-07-31 22:01:33 +0530

    Add Layout component with Sidebar and Outlet

---
+++ client/src/components/Layout.tsx
@@ -0,0 +1,17 @@
+import { Outlet, useLocation } from 'react-router-dom';
+import Sidebar from './Sidebar';
+
+export default function Layout() {
+  const location = useLocation();
+  
+  return (
+    <div className="flex h-screen bg-gray-50">
+      <Sidebar />
+      <main className="flex-1 overflow-auto p-8">
+        <div className="max-w-7xl mx-auto">
+          <Outlet />
+        </div>
+      </main>
+    </div>
+  );
+}
